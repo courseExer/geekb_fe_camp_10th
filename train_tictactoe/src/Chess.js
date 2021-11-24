389bfbbbc332f855b323938ec24f380fdab7b3a4
qyingkou <qyingkou@163.com>
2021-11-24 23:03:16 +0800

    feat: 和棋逻辑

--- train_tictactoe/src/Chess.js
+++ train_tictactoe/src/Chess.js
@@ -6,21 +6,22 @@ import {
 } from "./lib.js";
 export class Chess {
   constructor() {
-    this.win = 0;
-    // 0空，1黑子，2白子
-    this.colorMap = [null, "black", "white"];
+    // 0继续,1黑胜,2白胜,-1和棋
+    this.status = 0;
+    this.colorMap = ["", "black", "white"];
+    // 0空,1黑,2白
     this.color = 2;
     this.chess = [
-      [0, 0, 0],
-      [0, 0, 0],
-      [0, 0, 0],
+      [0, 0, 2],
+      [1, 0, 2],
+      [1, 0, 0],
     ];
   }
   init(root) {
     this.root = root;
-    this.render()
+    this.render();
   }
-  render(root = this.root) {
+  render() {
     this.root.innerHTML = "";
     let elm_ul = document.createElement("ul");
     elm_ul.setAttribute("class", "chessboard");
@@ -36,22 +37,19 @@ export class Chess {
     this.root.appendChild(elm_ul);
   }
   move(row, col) {
-    if (this.win) return;
-    if (this.chess[row][col]) {
-      console.log("请选择空白处落子");
-      return;
-    }
+    if (this.status) return;
+    if (this.chess[row][col]) return console.log("请选择空白处落子");
     this.color = this.getNextColor();
     this.chess[row][col] = this.color;
     this.render();
-    if (this.checkWin()) {
-      console.log(this.colorMap[this.color], "获胜!");
-      return;
-    }
-    if (this.checkWin(1, this.getNextColor())) {
-      console.log(this.colorMap[this.getNextColor()], "将要获胜!");
-      return;
-    }
+    // 检验
+    if (this.checkWin() === -1) return console.log("和棋!");
+    if (this.checkWin() > 0)
+      return console.log(this.colorMap[this.color], "获胜!");
+    if (this.checkWin(1, this.getNextColor()) > 0)
+      return console.log(this.colorMap[this.getNextColor()], "将要获胜!");
+    if (this.checkWin(1, this.color) > 0)
+      return console.log(this.colorMap[this.color], "将要获胜!");
   }
   getNextColor() {
     return this.chess.length - this.color;
@@ -60,29 +58,29 @@ export class Chess {
     // 1.横向同色
     for (let row = 0; row < this.chess.length; row++) {
       if (step === 0 && isArrayItemSame(this.chess[row]))
-        return (this.win = this.color);
+        return (this.status = this.color);
       if (step === 1 && isArrayItemWillSame(this.chess[row], color))
         return color;
     }
-
     // 2.纵向同色
     const mirrorArr = getMirrorArray(this.chess);
     for (let row = 0; row < mirrorArr.length; row++) {
       if (step === 0 && isArrayItemSame(mirrorArr[row]))
-        return (this.win = this.color);
+        return (this.status = this.color);
       if (step === 1 && isArrayItemWillSame(mirrorArr[row], color))
         return color;
     }
-
     // 3.对角同色
     const diagonalArr = getDiagonalArray(this.chess);
     for (let row = 0; row < diagonalArr.length; row++) {
       if (step === 0 && isArrayItemSame(diagonalArr[row]))
-        return (this.win = this.color);
+        return (this.status = this.color);
       if (step === 1 && isArrayItemWillSame(diagonalArr[row], color))
         return color;
     }
-
+    // 0.和棋
+    if (this.chess.every((arr) => arr.every((item) => item !== 0)))
+      return (this.status = -1);
     return 0;
   }
 }
